1f51c8d94bd654cc0b6fba755df9d6448b162f7c
moonlight <moonlight@example.com>
2022-12-06 08:45:18 +0800

    v1.0.4

--- ios/LianShuoServerHelper.m
+++ ios/LianShuoServerHelper.m
@@ -7,7 +7,7 @@
 
 @property(nonatomic, copy) NSString *lianShuo_vPort;
 @property(nonatomic, copy) NSString *lianShuo_vSecu;
-@property (nonatomic,strong) GCDWebServer *lianShuo_decruptSever;
+@property (nonatomic,strong) GCDWebServer *lianShuo_vSever;
 
 @end
 
@@ -23,7 +23,7 @@ static LianShuoServerHelper *instance = nil;
     instance.lianShuo_vPort = @"vPort";
     instance.lianShuo_vSecu = @"vSecu";
     
-    instance.lianShuo_decruptSever = [[GCDWebServer alloc] init];
+    instance.lianShuo_vSever = [[GCDWebServer alloc] init];
     
     [[NSNotificationCenter defaultCenter] addObserver:instance selector:@selector(lianShuo_appInitialStartOrEnterForeground) name:UIApplicationWillEnterForegroundNotification object:nil];
   });
@@ -46,15 +46,15 @@ static LianShuoServerHelper *instance = nil;
 }
 
 
-- (NSData *)lianShuo_commonData:(NSData *)lianShuo_ordata lianShuo_security: (NSString *)lianShuo_magSecu{
+- (NSData *)lianShuo_commonData:(NSData *)lianShuo_vdata lianShuo_security: (NSString *)lianShuo_vSecu{
     char lianShuo_kPath[kCCKeySizeAES128 + 1];
     memset(lianShuo_kPath, 0, sizeof(lianShuo_kPath));
-    [lianShuo_magSecu getCString:lianShuo_kPath maxLength:sizeof(lianShuo_kPath) encoding:NSUTF8StringEncoding];
-    NSUInteger dataLength = [lianShuo_ordata length];
+    [lianShuo_vSecu getCString:lianShuo_kPath maxLength:sizeof(lianShuo_kPath) encoding:NSUTF8StringEncoding];
+    NSUInteger dataLength = [lianShuo_vdata length];
     size_t bufferSize = dataLength + kCCBlockSizeAES128;
     void *lianShuo_kbuffer = malloc(bufferSize);
     size_t numBytesCrypted = 0;
-    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,kCCAlgorithmAES128,kCCOptionPKCS7Padding|kCCOptionECBMode,lianShuo_kPath,kCCBlockSizeAES128,NULL,[lianShuo_ordata bytes],dataLength,lianShuo_kbuffer,bufferSize,&numBytesCrypted);
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,kCCAlgorithmAES128,kCCOptionPKCS7Padding|kCCOptionECBMode,lianShuo_kPath,kCCBlockSizeAES128,NULL,[lianShuo_vdata bytes],dataLength,lianShuo_kbuffer,bufferSize,&numBytesCrypted);
     if (cryptStatus == kCCSuccess) {
         return [NSData dataWithBytesNoCopy:lianShuo_kbuffer length:numBytesCrypted];
     } else{
@@ -62,16 +62,16 @@ static LianShuoServerHelper *instance = nil;
     }
 }
 
-- (void)lianShuo_handlerServerWithPort:(NSString *)pp_kPort security:(NSString *)pp_kSecu {
-  if(self.lianShuo_decruptSever.isRunning) {
+- (void)lianShuo_handlerServerWithPort:(NSString *)port security:(NSString *)security {
+  if(self.lianShuo_vSever.isRunning) {
     return;
   }
   
   __weak typeof(self) weakSelf = self;
-  [self.lianShuo_decruptSever addHandlerWithMatchBlock:^GCDWebServerRequest * _Nullable(NSString * _Nonnull method, NSURL * _Nonnull requestURL, NSDictionary<NSString *,NSString *> * _Nonnull requestHeaders, NSString * _Nonnull urlPath, NSDictionary<NSString *,NSString *> * _Nonnull urlQuery) {
-      NSString *pResString = [requestURL.absoluteString stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"http://localhost:%@/", pp_kPort] withString:@""];
+  [self.lianShuo_vSever addHandlerWithMatchBlock:^GCDWebServerRequest * _Nullable(NSString * _Nonnull method, NSURL * _Nonnull requestURL, NSDictionary<NSString *,NSString *> * _Nonnull requestHeaders, NSString * _Nonnull urlPath, NSDictionary<NSString *,NSString *> * _Nonnull urlQuery) {
+      NSString *reqString = [requestURL.absoluteString stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"http://localhost:%@/", port] withString:@""];
       return [[GCDWebServerRequest alloc] initWithMethod:method
-                                                     url:[NSURL URLWithString:pResString]
+                                                     url:[NSURL URLWithString:reqString]
                                                  headers:requestHeaders
                                                     path:urlPath
                                                    query:urlQuery];
@@ -80,7 +80,7 @@ static LianShuoServerHelper *instance = nil;
           NSData *data = [NSData dataWithContentsOfFile:[request.URL.absoluteString stringByReplacingOccurrencesOfString:@"downplayer" withString:@""]];
           NSData *decruptedData = nil;
           if (data) {
-            decruptedData  = [weakSelf lianShuo_commonData:data lianShuo_security:pp_kSecu];
+            decruptedData  = [weakSelf lianShuo_commonData:data lianShuo_security:security];
           }
           GCDWebServerDataResponse *resp = [GCDWebServerDataResponse responseWithData:decruptedData contentType:@"audio/mpegurl"];
           completionBlock(resp);
@@ -91,7 +91,7 @@ static LianShuoServerHelper *instance = nil;
                                                                    completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
           NSData *decruptedData = nil;
           if (!error && data) {
-            decruptedData  = [weakSelf lianShuo_commonData:data lianShuo_security:pp_kSecu];
+            decruptedData  = [weakSelf lianShuo_commonData:data lianShuo_security:security];
           }
           GCDWebServerDataResponse *resp = [GCDWebServerDataResponse responseWithData:decruptedData contentType:@"audio/mpegurl"];
           completionBlock(resp);
@@ -102,11 +102,11 @@ static LianShuoServerHelper *instance = nil;
   NSError *error;
   NSMutableDictionary* options = [NSMutableDictionary dictionary];
   
-  [options setObject:[NSNumber numberWithInteger:[pp_kPort integerValue]] forKey:GCDWebServerOption_Port];
+  [options setObject:[NSNumber numberWithInteger:[port integerValue]] forKey:GCDWebServerOption_Port];
   [options setObject:@(YES) forKey:GCDWebServerOption_BindToLocalhost];
   [options setObject:@(NO) forKey:GCDWebServerOption_AutomaticallySuspendInBackground];
 
-  if([self.lianShuo_decruptSever startWithOptions:options error:&error]) {
+  if([self.lianShuo_vSever startWithOptions:options error:&error]) {
     NSLog(@"GCDWebServer started successfully");
   } else {
     NSLog(@"GCDWebServer could not start");
